b11779ee136b181b8692494d9075f8c431054ae3
f-bravo <bravoback@outlook.com>
2024-02-29 12:45:04 -0300

    App.css general style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
 
-import { Article, Brand, CTA } from './components';
-import { Blog, Feature, Footer, Header, Possibility, WhatGPT3} from './containers';
+import { Article, Brand, CTA, Navbar } from './components';
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3} from './containers';
+
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div className='App'>
+        <div className='gradient__bg'>
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatGPT3/>
+        <Features/>
+        <Possibility/>
+        <CTA/>
+        <Blog/>
+        <Footer/>
     </div>
   )
 }
